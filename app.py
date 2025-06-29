8433b94b122a304b9b1816c17882d91790a7bf9d
MAHMOUDoss <mdm73596@gmail.com>
2025-06-29 23:37:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ تطبيق محمود شغال الآن!"
-
-if __name__ == "__main__":
-    app.run()
+    return "✅ تطبيق محمود شغال الآن على Render!"
