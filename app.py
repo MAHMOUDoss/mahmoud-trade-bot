965bdf6ec190d0c67504a4fd54f966eb7457cc08
MAHMOUDoss <mdm73596@gmail.com>
2025-06-29 23:16:19 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ تطبيق محمود شغال الآن على Heroku!"
+    return "✅ تطبيق محمود شغال الآن!"
 
 if __name__ == "__main__":
     app.run()
